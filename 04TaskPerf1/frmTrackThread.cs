38a16c2cacca007bb7a0437d5ea2b7293ff72f53
jankiel09 <ezekielambat@gmail.com>
2025-09-29 19:39:15 +0800

    UPDATE: 1. Created frmTrackThread window forms. 2. Designed the UI of the window forms. 3. Created MyThreadClass 4. Declared 2 static methods named Thread1 and Thread2. 5. Created loop that loops 2 times in Thread1. Suspen it in 500ms or 0.5 seconds. and added the following code provided in the moduel 6. In thread2, I created a loop of six times and suspend the thread for 1500ms or 1.5 seconds. 7. frm TrackThreadClass, created 4 threads named threadA, threadB, threadC, threadD. 8. Set the priority according to the module. 9. Used join to stoip calling the method.

--- 04TaskPerf1/frmTrackThread.cs
+++ 04TaskPerf1/frmTrackThread.cs
@@ -32,7 +32,55 @@ namespace _04TaskPerf1
 
         private void button1_Click(object sender, EventArgs e)
         {
-           
+            // greyed out the run button to isolate the process. prevent multiple clicks.
+            btnStart.Enabled = false;
+            lblStatus.Text = "-Thread Starts-";
+
+            Debug.WriteLine("-Thread Starts-");
+            System.Diagnostics.Trace.WriteLine("-Thread Starts-");
+
+            ThreadStart threadStart1 = new ThreadStart(MyThreadClass.Thread1);
+            ThreadStart threadStart2 = new ThreadStart(MyThreadClass.Thread2);
+            ThreadStart threadStart3 = new ThreadStart(MyThreadClass.Thread1);
+            ThreadStart threadStart4 = new ThreadStart(MyThreadClass.Thread2);
+
+
+            // create 4 threads named ThreadA and ThreadB
+            ThreadA = new Thread(threadStart1);
+            ThreadB = new Thread(threadStart2);
+            ThreadC = new Thread(threadStart3);
+            ThreadD = new Thread(threadStart4);
+
+            // set thread names
+            ThreadA.Name = "ThreadA";
+            ThreadB.Name = "ThreadB";
+            ThreadC.Name = "ThreadC";
+            ThreadD.Name = "ThreadD";
+
+            Console.WriteLine("Starting ThreadA, ThreadB, ThreadC, ThreadD");
+            Console.WriteLine("================================");
+
+            ThreadA.Start();
+            ThreadB.Start();
+            ThreadC.Start();
+            ThreadD.Start();
+
+            ThreadA.Join();
+            ThreadB.Join();
+            ThreadC.Join();
+            ThreadD.Join();
+
+            string endMessage = "-End of Thread-";
+            Debug.WriteLine(endMessage);
+            System.Diagnostics.Trace.WriteLine(endMessage);
+            Console.WriteLine(endMessage);
+
+            Console.WriteLine("================================");
+            Console.WriteLine("Both threads have completed.");
+
+            // update the label to show threads have ended
+            lblStatus.Text = "-End Of Thread-";
+            btnStart.Enabled = true; // un-grey the run button.
         }
     }
 }
